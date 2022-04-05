ef1f375ba961ca09533cd7cfc9c74f456c8dac4b
Hiexy <45509563+Hiexy@users.noreply.github.com>
2022-04-05 11:23:23 +0300

    Fix issue with scanning module

--- Viper/scan/scan.py
+++ Viper/scan/scan.py
@@ -5,7 +5,7 @@ def scan_aps(interface):
     Run a scan to show all available access points.
     """
     airmon = pyrcrack.AirmonNg()
-    async with airmon("wlp3s0") as mon:
+    async with airmon(interface) as mon:
         async with pyrcrack.AirodumpNg() as pdump:
             async for aps in pdump(mon.monitor_interface):
                 break # So notebook execution doesn't get stuck here 
@@ -16,7 +16,7 @@ def scan_ap(interface, ap):
     """
     Scan a specific access point and return more results.
     """
-    async with airmon("wlp3s0") as mon:
+    async with airmon(interface) as mon:
     # Re-set the interface in monitor mode as in 
     # previous execution it would have been cleaned up
         async with pyrcrack.AirodumpNg() as pdump:
