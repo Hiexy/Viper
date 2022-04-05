efa055c015df9838c42d0c51182f40d58b21e5ce
Hiexy <45509563+Hiexy@users.noreply.github.com>
2022-04-05 11:28:32 +0300

    Create Initial Module

--- Viper/viper.py
+++ Viper/viper.py
@@ -1,7 +1,21 @@
+from viper.scan.scan import scan_aps, scan_ap
 
 class Viper:
     def __init__(self):
         self.interface = self.init_interface()
 
     def init_interface(self):
+        """
+        Initiate wireless network interface for monitor mode.
+        """
         pass
+    
+    def scan(self):
+        """
+        Scan all available access points and return all available information.
+        """
+        aps = scan_aps(self.interface)
+        for ap in aps:
+            result_ap = scan_ap(interface, ap)
+            print(result_ap.asdict())
+        
